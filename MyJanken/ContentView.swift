bc98115222c758c820e0892cf4744c2d1587ba66
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-15 20:13:28 +0900

    グーの処理記述

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        // グーの画像指定
-        Image("gu")
-            // リサイズを指定
-            .resizable()
-            // 画面に収まるようにアスペクト比を維持する指定
-            .aspectRatio(contentMode: .fit)
+  var body: some View {
+        
+      VStack {
+            // グーの画像指定
+            Image("gu")
+                // リサイズを指定
+                .resizable()
+                //画面に収まるようにアスペクト比を維持す定
+                .aspectRatio(contentMode: .fit)
+            //じゃんけんの種類を指定
+            Text("グー")
+            // じゃんけんをするボタン！！
+            Button(action: {
+                // buttonがタップされた時の動き
+                print("タップされたよ!!")
+            }) {
+                // ボタンに表示する文字を指定
+                Text("じゃんけんをする")
+            }
+        }
     }
 }
 
