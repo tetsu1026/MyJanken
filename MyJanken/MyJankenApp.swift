23a1e05652d6eefb019fe42065e2913c04e441a6
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-15 19:06:21 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Tetsuro Watanabe on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
